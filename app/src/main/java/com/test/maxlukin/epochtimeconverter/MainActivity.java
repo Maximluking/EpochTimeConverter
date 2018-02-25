9336888703a1fcda7c6f10974c523b1e1c3a4f93
Max Lukin <maximluking@gmail.com>
2018-02-25 14:09:48 +0200

    Initial commit

---
+++ app/src/main/java/com/test/maxlukin/epochtimeconverter/MainActivity.java
@@ -0,0 +1,23 @@
+package com.test.maxlukin.epochtimeconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button0, button1, button2, button3, button4, button5, button6,
+            button7, button8, button9, buttonConvert, buttonChangeOrientationFValues, buttonHyphen,
+            buttonC, battonColon;
+
+    EditText valueOne, valueTwo;
+    
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
